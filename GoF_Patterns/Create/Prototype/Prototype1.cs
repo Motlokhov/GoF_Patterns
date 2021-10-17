061f551293de3f9163cc79ac266306f8fb1f4480
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-10-17 17:11:59 +0300

    Added prototype pattern

---
+++ GoF_Patterns/Create/Prototype/Prototype1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_Patterns.Create.Prototype
+{
+    internal class Prototype1 : IPrototype
+    {
+        private int _v1;
+        private string _v2;
+
+        public Prototype1(int v1,string v2)
+        {
+            _v1 = v1;
+            _v2 = v2;
+        }
+
+        public IPrototype Clone()
+        {
+            return new Prototype1(_v1, _v2);
+        }
+    }
+}
