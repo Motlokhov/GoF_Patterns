c951a820705adad06c872db6883711be012a3d05
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-10-17 13:03:44 +0300

    Added factory method pattern

---
+++ GoF_Patterns/Create/Factory Method/Factory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_Patterns.Create.Factory_Method
+{
+    internal class Factory : IMethod
+    {
+        public IProduct GetProduct()
+        {
+            return new FactoryProduct();
+        }
+    }
+}
---
+++ GoF_Patterns/Create/Factory Method/FactoryWithSomeProducts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_Patterns.Create.Factory_Method
+{
+    internal class FactoryWithSomeProducts : IMethod
+    {
+        private int _someParam;
+
+        public FactoryWithSomeProducts(int someParam)
+        {
+            _someParam = someParam;
+        }
+        public IProduct GetProduct()
+        {
+            switch(_someParam)
+            {
+                case 0: return new FactoryProduct();
+                case 1:return new OtherFactoryProduct();
+                case 100:return new OtherFactoryProduct();
+                default: return new FactoryProduct();
+            }
+        }
+    }
+}
